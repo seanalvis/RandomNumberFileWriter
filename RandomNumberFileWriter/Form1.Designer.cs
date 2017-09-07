9b8b84593b3e17916e8dfd17009fed6d0134faf0
Sean Alvis <me@seanalvis.com>
2017-09-06 20:53:18 -0400

    Finished and tested the application

--- RandomNumberFileWriter/Form1.Designer.cs
+++ RandomNumberFileWriter/Form1.Designer.cs
@@ -43,36 +43,39 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(187, 31);
+            this.label1.Location = new System.Drawing.Point(125, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(251, 25);
+            this.label1.Size = new System.Drawing.Size(184, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Random Number Generator";
             // 
             // showNumbersListBox
             // 
             this.showNumbersListBox.FormattingEnabled = true;
-            this.showNumbersListBox.ItemHeight = 20;
-            this.showNumbersListBox.Location = new System.Drawing.Point(113, 153);
+            this.showNumbersListBox.Location = new System.Drawing.Point(75, 99);
+            this.showNumbersListBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.showNumbersListBox.Name = "showNumbersListBox";
-            this.showNumbersListBox.Size = new System.Drawing.Size(402, 184);
-            this.showNumbersListBox.TabIndex = 1;
+            this.showNumbersListBox.Size = new System.Drawing.Size(269, 121);
+            this.showNumbersListBox.TabIndex = 0;
             // 
             // showNumbersButton
             // 
-            this.showNumbersButton.Location = new System.Drawing.Point(48, 387);
+            this.showNumbersButton.Location = new System.Drawing.Point(32, 252);
+            this.showNumbersButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.showNumbersButton.Name = "showNumbersButton";
-            this.showNumbersButton.Size = new System.Drawing.Size(113, 53);
+            this.showNumbersButton.Size = new System.Drawing.Size(75, 34);
             this.showNumbersButton.TabIndex = 2;
-            this.showNumbersButton.Text = "&Show Numbers";
+            this.showNumbersButton.Text = "&Display  Numbers";
             this.showNumbersButton.UseVisualStyleBackColor = true;
             this.showNumbersButton.Click += new System.EventHandler(this.showNumbersButton_Click);
             // 
             // clearButton
             // 
-            this.clearButton.Location = new System.Drawing.Point(192, 387);
+            this.clearButton.Location = new System.Drawing.Point(128, 252);
+            this.clearButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.clearButton.Name = "clearButton";
-            this.clearButton.Size = new System.Drawing.Size(106, 53);
+            this.clearButton.Size = new System.Drawing.Size(71, 34);
             this.clearButton.TabIndex = 3;
             this.clearButton.Text = "&Clear";
             this.clearButton.UseVisualStyleBackColor = true;
@@ -80,9 +83,10 @@
             // 
             // saveButton
             // 
-            this.saveButton.Location = new System.Drawing.Point(346, 387);
+            this.saveButton.Location = new System.Drawing.Point(231, 252);
+            this.saveButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.saveButton.Name = "saveButton";
-            this.saveButton.Size = new System.Drawing.Size(102, 53);
+            this.saveButton.Size = new System.Drawing.Size(68, 34);
             this.saveButton.TabIndex = 4;
             this.saveButton.Text = "&Save";
             this.saveButton.UseVisualStyleBackColor = true;
@@ -90,35 +94,40 @@
             // 
             // numOfNumbersTextBox
             // 
-            this.numOfNumbersTextBox.Location = new System.Drawing.Point(440, 93);
+            this.numOfNumbersTextBox.Location = new System.Drawing.Point(293, 60);
+            this.numOfNumbersTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.numOfNumbersTextBox.Name = "numOfNumbersTextBox";
-            this.numOfNumbersTextBox.Size = new System.Drawing.Size(100, 26);
+            this.numOfNumbersTextBox.Size = new System.Drawing.Size(68, 20);
             this.numOfNumbersTextBox.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(93, 96);
+            this.label2.Location = new System.Drawing.Point(62, 62);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(318, 20);
+            this.label2.Size = new System.Drawing.Size(213, 13);
             this.label2.TabIndex = 0;
             this.label2.Text = "Enter how many random numbers you want:";
             // 
             // exitButton
             // 
-            this.exitButton.Location = new System.Drawing.Point(490, 387);
+            this.exitButton.Location = new System.Drawing.Point(327, 252);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.exitButton.Name = "exitButton";
-            this.exitButton.Size = new System.Drawing.Size(95, 53);
-            this.exitButton.TabIndex = 7;
+            this.exitButton.Size = new System.Drawing.Size(63, 34);
+            this.exitButton.TabIndex = 5;
             this.exitButton.Text = "&Exit";
             this.exitButton.UseVisualStyleBackColor = true;
             this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AcceptButton = this.showNumbersButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(625, 593);
+            this.CancelButton = this.exitButton;
+            this.ClientSize = new System.Drawing.Size(417, 385);
             this.Controls.Add(this.exitButton);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.numOfNumbersTextBox);
@@ -127,8 +136,9 @@
             this.Controls.Add(this.showNumbersButton);
             this.Controls.Add(this.showNumbersListBox);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Random Number Generator";
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- RandomNumberFileWriter/Form1.cs
+++ RandomNumberFileWriter/Form1.cs
@@ -16,8 +16,6 @@ namespace RandomNumberFileWriter
         public Form1()
         {
             InitializeComponent();
-
-             //int numOfNums = int.Parse(numOfNumbersTextBox.Text);
         }
 
         // Create an application that writes random numbers to a file
@@ -25,74 +23,70 @@ namespace RandomNumberFileWriter
         // Let the user specify how many numbers to hold
         // Use a save dialog to set where file will be saved
 
-        
-
         private void showNumbersButton_Click(object sender, EventArgs e)
         {
+            // A try catch statement to catch and display any exceptions
             try
             {
+                // Set up the variables
                 int number;
                 Random rand = new Random();
                 int numOfNumbers = int.Parse(numOfNumbersTextBox.Text);
 
+                // Loop through the number set by user
                 for (int count = 0; count <= numOfNumbers; count++)
                 {
-
+                    // Each loop a random number is set and added to the listbox
                     number = rand.Next(100) + 1;
-                    //showNumbersListBox.Items.Add(number);
-                    StreamWriter outputFile;
-
-                    if (saveRandomNumFile.ShowDialog() == DialogResult.OK)
-                    {
-                        outputFile = File.CreateText(saveRandomNumFile.FileName);
-                        //outputFile = File.CreateText("five.txt");
-                    }
-                    else
-                    {
-                        MessageBox.Show("You clicked the Cancel button");
-                    }
+                    showNumbersListBox.Items.Add(number);
                 }
             }
+            // If there is an exception thrown display it in a messagebox
             catch(Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
         }
 
+        // On click of button Clear, clear the app
         private void clearButton_Click(object sender, EventArgs e)
         {
             numOfNumbersTextBox.Text = "";
             showNumbersListBox.Items.Clear();
         }
 
+        // On click of button Save, save the file
         private void saveButton_Click(object sender, EventArgs e)
         {
-            
-            StreamWriter outputFile;
-
-            //if (saveRandomNumFile.ShowDialog() == DialogResult.OK)
-            //{
-            //    outputFile = File.CreateText(saveRandomNumFile.FileName);
-            //    //outputFile = File.CreateText("five.txt");
-            //}
+            // Display a save dialog
             SaveFileDialog savefile = new SaveFileDialog();
-            // set a default file name
-            savefile.FileName = "unknown.txt";
-            // set filters - this can be done in properties as well
-            savefile.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            //// set a default file name
+            //savefile.FileName = "unknown.txt";
+            //// set filters - this can be done in properties as well
+            //savefile.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
 
+            // if Save is clicked
             if (saveRandomNumFile.ShowDialog() == DialogResult.OK)
             {
+                // Set up the StreamWriter to write to file
                 using (StreamWriter sw = new StreamWriter(saveRandomNumFile.FileName))
-                    
-                    sw.WriteLine("Hello World!");
+
+                    // For each item in the ListBox write a line to 
+                    // the new file you are saving
+                    foreach (var item in showNumbersListBox.Items)
+                    {
+                        sw.WriteLine(item);
+                    }                    
             }
+            // Else if Cancel is clicked display a message box letting user
+            // know what they have chosen
             else
             {
                 MessageBox.Show("You clicked the Cancel button");
             }
         }
 
+        // If exit is clicked close the application
         private void exitButton_Click(object sender, EventArgs e)
         {
             this.Close();
